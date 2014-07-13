8151b45492769371b517b01bca1efd513d933988
soydemirnaci <soydemir.naci@yahoo.com>
2014-07-13 16:08:19 +0300

    Son

--- src/com/omernaci/actionbarv1/MainActivity.java
+++ src/com/omernaci/actionbarv1/MainActivity.java
@@ -3,7 +3,9 @@ package com.omernaci.actionbarv1;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +13,36 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		case R.id.Omer:
+			
+			Toast.makeText(getApplicationContext(), "Omer", Toast.LENGTH_SHORT).show();
+			
+			break;
+
+		case R.id.Naci:
+			
+			Toast.makeText(getApplicationContext(), "Naci", Toast.LENGTH_SHORT).show();
+			
+			break;
+			
+		default:
+			break;
+		}
+		
+		return super.onOptionsItemSelected(item);
 	}
 }
